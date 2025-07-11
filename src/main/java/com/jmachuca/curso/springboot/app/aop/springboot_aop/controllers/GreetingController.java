e1c76b0658a71c65b935b5a351c88d07bd01dd58
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-10 20:26:10 -0400

    Creación del Proyecto. Services y Controlador implementado

---
+++ src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
@@ -0,0 +1,25 @@
+package com.jmachuca.curso.springboot.app.aop.springboot_aop.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jmachuca.curso.springboot.app.aop.springboot_aop.services.GreetingService;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class GreetingController {
+    
+    @Autowired
+    private GreetingService greetingService;
+    @GetMapping("/greeting")
+    public ResponseEntity<?> greeting() {
+        
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("Pepe", "Hola que tal!")));
+    }
+    
+}
---
+++ src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/services/GreetingService.java
@@ -0,0 +1,8 @@
+package com.jmachuca.curso.springboot.app.aop.springboot_aop.services;
+
+
+public interface GreetingService {
+
+    String sayHello(String person, String phrase);
+
+}
