9d36d8706ed1e6f5bcfe232ce0b33e26b5d5bdc1
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-23 22:14:32 -0400

    Se modifica mensaje de log y se agregar Advice After

--- src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.jmachuca.curso.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,12 +16,21 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.jmachuca.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))")
+    @Before("execution(* com.jmachuca.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
     public void loggerBefore(JoinPoint joinPoint) {
 
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
 
-        logger.info("Antes: " + method + " args: " + args);;
+        logger.info("Antes de ejecutar: {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), method, args);
+    }
+
+    @After("execution(* com.jmachuca.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Despues de ejecutar: {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), method, args);
     }
 }
