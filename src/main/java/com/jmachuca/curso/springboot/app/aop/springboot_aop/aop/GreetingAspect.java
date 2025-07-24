a755f609b431cb6eaa8da040547b87c96de7c6e3
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-23 23:04:50 -0400

    around

--- src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,9 +3,11 @@ package com.jmachuca.curso.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -19,7 +21,7 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut("execution(* com.jmachuca.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    @Pointcut("execution(* com.jmachuca.curso.springboot.app.aop.springboot_aop.services.*.*(..))")
     public void serviceLayerMethods() {}
 
     @Before("serviceLayerMethods()")
@@ -46,6 +48,34 @@ public class GreetingAspect {
         String args = Arrays.toString(joinPoint.getArgs());
 
         logger.info("Después de ejecutar y lanzar excepción: {}.{}() con argumentos: {} con excepción: {}", joinPoint.getSignature().getDeclaringTypeName(), method, args, exception.getMessage());
+    }
+
+    @After("serviceLayerMethods()")
+    public void logAfter(JoinPoint joinPoint) {
+        logger.info("Después de finalizar (siempre): {}.{}()",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName());
+    }
+
+    @Around("serviceLayerMethods()")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        Object result = null;
+
+        try {
+            logger.info("Around - Antes de ejecutar: {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), method, args);
+            result = joinPoint.proceed();
+            logger.info("Around - Después de ejecutar y retornar exitosamente: {}.{}() con resultado: {}", joinPoint.getSignature().getDeclaringTypeName(), method, result);
+
+            return result;
+        } catch (Throwable e) {
+            logger.info("Around - Después de ejecutar y lanzar excepción: {}.{}() con argumentos: {}", joinPoint.getSignature().getDeclaringTypeName(), method, args);
+            throw e;
+        }
+
     }
 
 }
