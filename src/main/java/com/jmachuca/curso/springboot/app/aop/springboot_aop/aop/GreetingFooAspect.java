6af96228a78c798b6249b157662425e55dc3351f
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-23 23:17:26 -0400

    Order para Aspectos

---
+++ src/main/java/com/jmachuca/curso/springboot/app/aop/springboot_aop/aop/GreetingFooAspect.java
@@ -0,0 +1,43 @@
+package com.jmachuca.curso.springboot.app.aop.springboot_aop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Component
+@Aspect
+public class GreetingFooAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("execution(* com.jmachuca.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public void serviceLayerMethods() {}
+
+    @Before("serviceLayerMethods()")
+    public void loggerBefore(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("GreetingFooAspect - Antes de ejecutar: {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), method, args);
+    }
+
+    @AfterReturning(pointcut = "serviceLayerMethods()", returning = "result")
+    public void loggerAfterReturning(JoinPoint joinPoint, Object result) {
+
+        String method = joinPoint.getSignature().getName();
+
+        logger.info("GreetingFooAspect - Después de ejecutar y retornar exitosamente: {}.{}() con resultado: {}", joinPoint.getSignature().getDeclaringTypeName(), method, result);
+    }
+
+}
